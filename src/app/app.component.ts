39362947bcfd3257b59e006a04b9deb000c4d59f
NadetDev <nadetdev@gmail.com>
2024-08-16 17:48:46 +0200

    drap drop example 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ng-drag-drop-01';
+
+  items = ['Item-1', 'Item-2', 'Item-3', 'Item-4'];
+
+  activeItems = ['Item-5', 'Item-6'];
+
+  doneItems = ['Item-7', 'Item-8', 'Item-9', 'Item-10'];
+
+  dropItems(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
